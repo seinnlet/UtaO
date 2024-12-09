d0b870d3451f0b8fcd67012d12008ec0e037e5e4
seinnlet <57383173+seinnlet@users.noreply.github.com>
2024-12-09 22:41:35 +0900

    update doremi script

--- js/doremi-script.js
+++ js/doremi-script.js
@@ -4,16 +4,18 @@ const pitchDisplay = document.getElementById('pitch');
 const startButton = document.getElementById('btn-doremi-start');
 
 const pitches = [
-	'C0', 'C#0', 'D0', 'D#0', 'E0', 'F0', 'F#0', 'G0', 'G#0', 'A0', 'A#0', 'B0',
-	'C1', 'C#1', 'D1', 'D#1', 'E1', 'F1', 'F#1', 'G1', 'G#1', 'A1', 'A#1', 'B1',
-	'C2', 'C#2', 'D2', 'D#2', 'E2', 'F2', 'F#2', 'G2', 'G#2', 'A2', 'A#2', 'B2',
-	'C3', 'C#3', 'D3', 'D#3', 'E3', 'F3', 'F#3', 'G3', 'G#3', 'A3', 'A#3', 'B3',
-	'C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4', 'G#4', 'A4', 'A#4', 'B4',
-	'C5', 'C#5', 'D5', 'D#5', 'E5', 'F5', 'F#5', 'G5', 'G#5', 'A5', 'A#5', 'B5',
-	'C6', 'C#6', 'D6', 'D#6', 'E6', 'F6', 'F#6', 'G6', 'G#6', 'A6', 'A#6', 'B6',
-	'C7', 'C#7', 'D7', 'D#7', 'E7', 'F7', 'F#7', 'G7', 'G#7', 'A7', 'A#7', 'B7',
-	'C8'
+	'C0', 'C#0', 'D0', 'D#0', 'E0', 'F0', 'F#0', 'G0', 'G#0', 'A0', 'A#0', 'B0',  // 0
+	'C1', 'C#1', 'D1', 'D#1', 'E1', 'F1', 'F#1', 'G1', 'G#1', 'A1', 'A#1', 'B1',  // 1
+	'C2', 'C#2', 'D2', 'D#2', 'E2', 'F2', 'F#2', 'G2', 'G#2', 'A2', 'A#2', 'B2',  // 2
+	'C3', 'C#3', 'D3', 'D#3', 'E3', 'F3', 'F#3', 'G3', 'G#3', 'A3', 'A#3', 'B3',  // 3
+	'C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4', 'G#4', 'A4', 'A#4', 'B4',  // 4
+	'C5', 'C#5', 'D5', 'D#5', 'E5', 'F5', 'F#5', 'G5', 'G#5', 'A5', 'A#5', 'B5',  // 5
+	'C6', 'C#6', 'D6', 'D#6', 'E6', 'F6', 'F#6', 'G6', 'G#6', 'A6', 'A#6', 'B6',  // 6
+	'C7', 'C#7', 'D7', 'D#7', 'E7', 'F7', 'F#7', 'G7', 'G#7', 'A7', 'A#7', 'B7',  // 7
+	'C8', 'C#8', 'D8', 'D#8', 'E8', 'F8', 'F#8', 'G8', 'G#8', 'A8', 'A#8', 'B8',  // 8
 ];
+
+console.log(pitches.length)
 
 const frequencies = [261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25];
 
@@ -70,8 +72,8 @@ function detectPitch() {
 function frequencyToPitch(frequency) {
 	if (frequency < 20 || frequency > 5000) return '-';
 
-	const noteIndex = Math.round(12 * (Math.log(frequency / 440) / Math.log(2))) + 69;
-	return pitches[noteIndex] || '-';
+	const noteIndex = Math.round( 12 * (Math.log( frequency / 440 )/Math.log(2) )) + 69;
+	return pitches[noteIndex - 12] || '-';
 }
 
 /**
@@ -187,8 +189,8 @@ function playNote(frequency) {
 	oscillator.frequency.setValueAtTime(frequency, audioContext.currentTime);
 
 	gainNode.gain.setValueAtTime(0, audioContext.currentTime);
-	gainNode.gain.linearRampToValueAtTime(0.8, audioContext.currentTime + 0.02);
-	gainNode.gain.linearRampToValueAtTime(0.6, audioContext.currentTime + 0.2);
+	gainNode.gain.linearRampToValueAtTime(0.96, audioContext.currentTime + 0.02);
+	gainNode.gain.linearRampToValueAtTime(0.7, audioContext.currentTime + 0.2);
 	gainNode.gain.setValueAtTime(0.6, audioContext.currentTime + 0.2);
 	gainNode.gain.linearRampToValueAtTime(0, audioContext.currentTime + 0.7);
 
